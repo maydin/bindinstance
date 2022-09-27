8fd6906f7473133f04716ee67ffb81b887e89fa3
murat aydin <maydin@gmail.com>
2022-09-27 15:43:17 +0200

    initial commit

---
+++ app/src/main/java/com/murat/bindinstance/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.murat.bindinstance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.murat.bindinstance.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
